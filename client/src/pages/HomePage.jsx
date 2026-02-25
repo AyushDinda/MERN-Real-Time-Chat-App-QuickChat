9434e22fb652da7b70168d9b3a07959b5391dae7
Ayush Dinda <ayushdinda074@gmail.com>
2026-02-26 00:48:34 +0530

    Added MERN realtime chat app

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar.jsx'
+import ChatContainer from '../components/ChatContainer.jsx'
+import Right_SideBar from '../components/Right_SideBar.jsx'
+import { useContext } from 'react'
+import { ChatContext } from '../../context/ChatContext.jsx'
+
+const HomePage = () => {
+
+  // const [selectedUser, setSelectedUser] = useState(false);
+  const {selectedUser} = useContext(ChatContext)
+  return (
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+            <Sidebar />
+            <ChatContainer/>
+            <Right_SideBar />
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
